6c685a056f1e02c4d8c497840620b05a71a8a257
IvanMFreitas <ivan.freitas@outlook.com>
2023-12-04 22:23:53 -0300

    [ATM_Simulator] - Commit with coded solution and README

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿class Program
+{
+    /// <summary>
+    /// Main entrypoint
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        List<int> billDenominations = new List<int>() { 10, 50, 100 };
+
+        Console.Write("Enter the withdrawal amount: ");
+
+        // Read the user input as a string
+        string userInput = Console.ReadLine();
+
+        // Parse the string to an integer
+        if (int.TryParse(userInput, out int withdrawalAmount) && withdrawalAmount % 10 == 0)
+        {
+            List<List<int>> withdrawalCombinations = CalculateWithdrawalCombinations(billDenominations, withdrawalAmount);
+
+            Console.WriteLine($"For €{withdrawalAmount:N2} the available payout denominations would be:");
+            Console.WriteLine();
+            foreach (var combination in withdrawalCombinations)
+            {
+                //Group the results to print in a single line
+                var output = combination.GroupBy(n => n)
+                    .Select(group => $"{group.Count()} x €{group.Key:N2}");
+
+                // Print the results in a single line as requested
+                Console.WriteLine("· " + string.Join(" + ", output));
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number and ensure it is a multiple of 10.");
+        }
+
+        Console.WriteLine("Press Enter to exit.");
+        Console.ReadLine();
+    }
+
+    /// <summary>
+    /// Calculates the combinations of bills for a given withdrawal amount.
+    /// </summary>
+    /// <param name="billDenominations">The available bill denominations</param>
+    /// <param name="withdrawalAmount">The desired withdrawal amount</param>
+    /// <returns>A list of possible combinations of bills</returns>
+    static List<List<int>> CalculateWithdrawalCombinations(List<int> billDenominations, int withdrawalAmount)
+    {
+        List<List<int>> result = new List<List<int>>();
+        ExploreWithdrawalCombinations(new List<int>(), billDenominations, 0, 0, withdrawalAmount, result);
+        return result;
+    }
+
+    /// <summary>
+    /// Recursively explores possible combinations of bills for a given withdrawal amount.
+    /// </summary>
+    /// <param name="currentCombination">The current combination being explored</param>
+    /// <param name="billDenominations">The available bill denominations</param>
+    /// <param name="currentDenomination">The current bill denomination being considered</param>
+    /// <param name="currentSum">The current sum of bill denominations in the combination</param>
+    /// <param name="targetSum">The target sum (withdrawal amount)</param>
+    /// <param name="result">The list to store valid combinations</param>
+    static void ExploreWithdrawalCombinations(List<int> currentCombination, List<int> billDenominations, int currentDenomination, int currentSum, int targetSum, List<List<int>> result)
+    {
+        // Check if we have reached the target sum
+        if (currentSum == targetSum)
+        {
+            // If the current sum matches the target sum, add the current combination to the result list
+            result.Add(currentCombination.ToList());
+            return; // Exit the current exploration branch
+        }
+
+        // Check if the current sum exceeds the target sum
+        if (currentSum > targetSum)
+        {
+            return; // Exit the current exploration branch as the sum is already greater than the target
+        }
+
+        // Explore combinations with different bill denominations
+        foreach (int denomination in billDenominations.Where(denom => denom >= currentDenomination))
+        {
+            // Create a copy of the current combination and add the current denomination to it
+            List<int> updatedCombination = currentCombination.ToList();
+            updatedCombination.Add(denomination);
+
+            // Recursively explore combinations with the updated combination
+            ExploreWithdrawalCombinations(updatedCombination, billDenominations, denomination, currentSum + denomination, targetSum, result);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# DenominationRoutine
-An ATM Simulator
+# Coding test .NET
+This is the first part of the test, simulating an ATM Machine.
+
+## Requests
+
+- Should have a list of bills that is allowed to the customer draw. In this example, we should use 10.00, 50.00, and 100.00 money bills;
+- It should return the combination of money bills, using bills that was set at the beginning of the program;
+- It should wait for the user Inputs the amount of cash to draw;
+
+## Used approach
+
+The easiest way to make it work is to make the method that calculates the return possibilities works recursivelly.
+So, every call of this method, should check:
+- First: If the amount of withdraw is already reached;
+- Second: Check if the amount passed the limit;
+- Third: Checks the denomination, related to the current denomination, and for every denomination, the method should be called and checked again (the first and second step);
